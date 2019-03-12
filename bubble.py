3a5b2dd6fa9099c7893bc5c4d08e319605cbc2a1
koji <xxxkojiyyy@gmail.com>
2019-03-12 18:29:24 +0900

    バブルソードの写経用ファイルです

--- bubble.py
+++ bubble.py
@@ -9,11 +9,12 @@ def main():
     result = bubble(data)
 
     print(result)
-    
+
     return 
 
 def bubble(data):
-
+    #昇順に数字を並べ替えます
+    
     for i in range(len(data)):#0~9
         for j in range(len(data)-1,i,-1):
             if data[j] < data[j-1]:
