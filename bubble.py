d0538753a8c83bdde239c1ea021262b5555dcea0
koji <xxxkojiyyy@gmail.com>
2019-03-12 17:47:30 +0900

    バブルソートのコミット

---
+++ bubble.py
@@ -0,0 +1,28 @@
+
+
+
+
+def main():
+    data = [3,5,2,1,4,6,8,9,7]
+
+    print("デバッグが動きます")
+    x = range(len(data))
+    print("デバッグ")
+
+    result = bubble(data)
+
+    print(result)
+    return 
+
+def bubble(data):
+
+    for i in range(len(data)):#0~9
+        for j in range(len(data)-1,i,-1):
+            if data[j] < data[j-1]:
+                data[j],data[j-1] = data[j-1], data[j]
+                
+ return data
+
+
+if __name__ == "__main__":
+    main()
